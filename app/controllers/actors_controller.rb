db562fd193c473f81a1f2a3f66e040c8119b89a2
Kevin Zhong <kvnzhong@gmail.com>
2022-05-09 21:19:18 -0700

    Added examples using URL segment/query/body parameters.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -3,4 +3,18 @@ class ActorsController < ApplicationController
     actor = Actor.find_by(id: params["id"])
     render json: actor.as_json
   end
+
+  def qshow
+    actor = Actor.find_by(first_name: params["first_name"])
+    render json: actor.as_json
+  end
+
+  def create
+    actor = Actor.create(
+      first_name: params["first_name"],
+      last_name: params["last_name"],
+      known_for: params["known_for"],
+    )
+    render json: actor.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/actors/:id", controller: "actors", action: "show"
+  get "/actors/:id", controller: "actors", action: "show" # This is the URL segment parameter
+  get "/actors", controller: "actors", action: "qshow"  # This is the query parameter
+  post "/actors", controller: "actors", action: "create" # This is the body parameter
   get "/movies", controller: "movies", action: "index"
   get "/movies/:id", controller: "movies", action: "show"
 end
